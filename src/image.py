f8d5922aa7aea6a9e759ed2588ec0b66bf68430f
RamonMachado <ramon.machado2013@hotmail.com>
2021-05-02 19:17:31 -0300

    Created histogram functions to image module

--- src/image.py
+++ src/image.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import numpy as np
 
 def readImage(path):
     return  cv.imread(path,0)
@@ -11,3 +12,9 @@ def cropImageHorizontal(img, start, end):
 
 def cropImageVertical(img, start, end):
     return img[:, start:end]
+    
+def getHorizontalHistogram(img):
+    return img.shape[1] - np.sum(img,axis=1,keepdims=True)/255
+
+def getVerticalHistogram(img):
+    return img.shape[0] - np.sum(img,axis=0,keepdims=True)/255
